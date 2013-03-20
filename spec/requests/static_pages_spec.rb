b85c9b9546766215bc87e34cba03ea84386f01a2
Andreas Maier <andreas.maier@gmail.com>
2013-03-19 21:06:59 -0700

    static page tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,21 +5,40 @@ describe "StaticPages" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector( 'h1', :text => 'Sample App')
     end
-  end
+
+    it 'should have the right title "Home"' do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                  :text => "Ruby on Rails Tutorial Sample App | Home")
+      end
+    end
 
   describe 'Help Page' do
     it 'should have content "Help"' do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
     end
+
+    it "should have the right title 'Help" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                                :text => "Ruby on Rails Tutorial Sample App | Help")
+      end
+
   end
 
-  describe  do
+  describe 'About Page' do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector( 'h1', :text => 'About Us')
+    end
+
+    it "should have the right title 'About Us" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                                :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
