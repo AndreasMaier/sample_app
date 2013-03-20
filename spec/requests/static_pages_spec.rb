1cb7b0b6a46dc134b027a46819d2dabf811dab91
Andreas Maier <andreas.maier@gmail.com>
2013-03-19 20:26:49 -0700

    First tests done

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home Page" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      page.should have_content('Sample App')
+    end
+  end
+
+  describe 'Help Page' do
+    it 'should have content "Help"' do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+
+  describe  do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+  end
+end
