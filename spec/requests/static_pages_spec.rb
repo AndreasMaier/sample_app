0061c93a5081d8eed7d779a16b919cdf4e907fcf
Andreas Maier <andreas.maier@gmail.com>
2013-03-21 22:01:44 -0700

    Add a full title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,15 +5,21 @@ describe "StaticPages" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      page.should have_selector( 'h1', :text => 'Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it 'should have the right title "Home"' do
+    it 'should have the base title' do
       visit '/static_pages/home'
       page.should have_selector('title',
-                  :text => "Ruby on Rails Tutorial Sample App | Home")
-      end
+                                :text => "Ruby on Rails Tutorial Sample App")
     end
+
+    it 'should not have the right title "Home"' do
+      visit '/static_pages/home'
+      page.should_not have_selector('title',
+                                :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
 
   describe 'Help Page' do
     it 'should have content "Help"' do
@@ -25,14 +31,14 @@ describe "StaticPages" do
       visit '/static_pages/help'
       page.should have_selector('title',
                                 :text => "Ruby on Rails Tutorial Sample App | Help")
-      end
+    end
 
   end
 
   describe 'About Page' do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector( 'h1', :text => 'About Us')
+      page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the right title 'About Us" do
